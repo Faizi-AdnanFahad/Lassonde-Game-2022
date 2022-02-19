f41aeba901240e96968c7e67a431efe69716885d
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-02-18 21:56:22 -0500

    Main Page and age page

---
+++ app/src/main/java/eecs1022/lab7/lassondgameapp/MainActivity.java
@@ -0,0 +1,14 @@
+package eecs1022.lab7.lassondgameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
