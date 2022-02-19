ef76d2e8791a2962f96ed5bfeeefb0e440ec0338
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-02-18 22:23:38 -0500

    UI Stored

--- app/src/main/java/eecs1022/lab7/lassondgameapp/MainActivity.java
+++ app/src/main/java/eecs1022/lab7/lassondgameapp/MainActivity.java
@@ -2,13 +2,29 @@ package eecs1022.lab7.lassondgameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // changing from loginMainPage -> loginPage
+        this.login = findViewById(R.id.buttonSignUp);
+        this.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AgeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
